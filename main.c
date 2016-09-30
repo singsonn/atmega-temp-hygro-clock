bf38ab08f15609a25fbebe23c21ce79c5ba007cf
Axel Caspard <axel@singsonn.com>
2016-09-30 13:11:47 -0400

    Output values to LEDs + fuse change command + one-line compile command

--- main.c
+++ main.c
@@ -7,30 +7,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void display_digit(int8_t digit, uint8_t decimal_point_bit);
+void display_digit(uint8_t digit, uint8_t decimal_point_bit);
 void ports_init(void);
 void interrupts_init(void);
+void timer_init(void);
 void display_toggle(uint8_t toggle_bit);
 
+volatile uint8_t toggle = 0;
+volatile uint8_t dp_off = 0;
+volatile uint8_t dp_on = 1;
 
-int main(void) {
+volatile uint8_t one = 1;
+volatile uint8_t two = 2;
+volatile uint8_t three = 3;
+volatile uint8_t h = 11;
+
+int main(void) { // main program
 
   ports_init();
   interrupts_init();
+  timer_init();
 
+  sei(); // Enable global interrupts
+
+  while(1){
+  }
 }
 
-//Interrupt Service Routine for INT1
+//Interrupt Service Routine for INT1 : Toggle between functions (show temperature, humidity, time, date, year)
 ISR(INT1_vect) {
 }
 
-
-void display_digit(int8_t digit, uint8_t decimal_point_bit) {
-  if (decimal_point_bit == 1){
-    PORTD |= (1<<PD0);
+ISR(TIMER1_OVF_vect){
+  if (toggle == 0){ // All Displays off
+    display_toggle(toggle);
+    toggle = 1;
+  }else if (toggle == 1){ // Display 1 (most left) ON
+    display_toggle(toggle);
+    display_digit(one,dp_off);
+    toggle = 2;
+  }else if (toggle == 2){ // Display 2 (middle left) ON
+    display_toggle(toggle);
+    display_digit(two,dp_on);
+    toggle = 3;
+  }else if (toggle == 3){ // Display 3 (middle right) ON
+    display_toggle(toggle);
+    display_digit(three,dp_off);
+    toggle = 4;
+  }else if (toggle == 4){ // Display 4 (most right) ON
+    display_toggle(toggle);
+    display_digit(h,dp_on);
+    toggle = 1;
+  }else{ // If toggle is not equal any of 0,1,2,3,4, then make it equal to 0
+    toggle = 0;
   }
-  else{
-    PORTD &= ~(1<<PD0);
+}
+
+void display_digit(uint8_t digit, uint8_t decimal_point_bit) {
+  switch(decimal_point_bit){
+    case 0:
+      PORTD &= ~(1<<PD0);
+      break;
+    case 1:
+      PORTD |= (1<<PD0);
+      break;
+    default:
+      PORTD &= ~(1<<PD0);
+      break;
   }
   switch(digit){
     case 0: // Display Number 0
@@ -45,8 +88,8 @@ void display_digit(int8_t digit, uint8_t decimal_point_bit) {
       PORTD &= ~(1<<PD4);
       break;
     case 2: // Display Number 2
-      PORTB |= (1<<PB2);
-      PORTB &= ~(1<<PB1);
+      PORTB |= (1<<PB1);
+      PORTB &= ~(1<<PB2);
       PORTC |= (1<<PC0) | (1<<PC1) | (1<<PC3);
       PORTC &= ~(1<<PC2);
       PORTD |= (1<<PD4);
@@ -93,6 +136,18 @@ void display_digit(int8_t digit, uint8_t decimal_point_bit) {
       PORTC |= (1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3);
       PORTD |= (1<<PD4);
       break;
+    case 10: // Display char "C"
+      PORTB |= (1<<PB1) | (1<<PB2);
+      PORTC |= (1<<PC0) | (1<<PC3);
+      PORTC &= ~((1<<PC1) | (1<<PC2));
+      PORTD &= ~(1<<PD4);
+      break;
+    case 11: // Display char "H"
+      PORTB |= (1<<PB1) | (1<<PB2);
+      PORTC |= (1<<PC1) | (1<<PC2);
+      PORTC &= ~((1<<PC0) | (1<<PC3));
+      PORTD |= (1<<PD4);
+      break;
     default: // Display Number 8
       PORTB |= (1<<PB1) | (1<<PB2);
       PORTC |= (1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3);
@@ -102,12 +157,12 @@ void display_digit(int8_t digit, uint8_t decimal_point_bit) {
 }
 
 void ports_init(void){
-  DDRB = (1<<DDB0) | (1<<DDB1) | (1<<DDB2); // Set portB0-1-2 as output
-  DDRC = (1<<DDC0) | (1<<DDC1) | (1<<DDC2) | (1<<DDC3); // Set PortC0-1-2-3 as output
-  DDRD = (1<<DDD0) | (1<<DDD4) | (1<<DDD5) | (1<<DDD6) | (1<<DDD7); // Set PortD0-4-5-6-7 as output
-  PORTB = ~((1<<PB0) | (1<<PB1) | (1<<PB2)); // Initialize PORTB 0-1-2 at 0
-  PORTC = ~((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3)); // Initialize PORTC0-1-2-3 at 0
-  PORTD = ~((1<<PD0) | (1<<PD4) | (1<<PD5) | (1<<PD6) | (1<<PD7)); // Initialize PORTD0-4-5-6-7 at 0
+  DDRB |= (1<<DDB0) | (1<<DDB1) | (1<<DDB2); // Set portB0-1-2 as output
+  DDRC |= (1<<DDC0) | (1<<DDC1) | (1<<DDC2) | (1<<DDC3); // Set PortC0-1-2-3 as output
+  DDRD |= (1<<DDD0) | (1<<DDD4) | (1<<DDD5) | (1<<DDD6) | (1<<DDD7); // Set PortD0-4-5-6-7 as output
+  PORTB &= ~((1<<PB0) | (1<<PB1) | (1<<PB2)); // Initialize PORTB 0-1-2 at 0
+  PORTC &= ~((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3)); // Initialize PORTC0-1-2-3 at 0
+  PORTD &= ~((1<<PD0) | (1<<PD4) | (1<<PD5) | (1<<PD6) | (1<<PD7)); // Initialize PORTD0-4-5-6-7 at 0
 }
 
 void interrupts_init(void){
@@ -115,32 +170,38 @@ void interrupts_init(void){
   EIMSK |= (1 << INT1);     // Turns on INT1 (for display toggle btw temp, humidity, time, date, year)
 }
 
+void timer_init(void){
+  TCCR1B |= (1 << CS10); // Set up timer with prescaler Fcpu/8
+  TCNT1 = 10; // Initialize Timer 1 counter at 0
+  TIMSK1 |= (1 << TOIE1); // Enable overflow interrupt
+}
+
 void display_toggle(uint8_t toggle_bit){
   switch(toggle_bit){
-    case 0:
+    case 0: // All 7-seg displays OFF
       PORTB &= ~(1<<PB0);
       PORTD &= ~((1<<PD5) | (1<<PD6) | (1<<PD7));
       break;
-    case 1:
+    case 1: // Top left ON, all others OFF
       PORTB |= (1<<PB0);
       PORTD &= ~((1<<PD5) | (1<<PD6) | (1<<PD7));
       break;
-    case 2:
+    case 2: // 2nd left (middle left) ON, all others OFF
       PORTB &= ~(1<<PB0);
       PORTD &= ~((1<<PD5) | (1<<PD6));
       PORTD |= (1<<PD7);
       break;
-    case 3:
+    case 3: // 2nd right (middle right) ON, all others OFF
       PORTB &= ~(1<<PB0);
       PORTD &= ~((1<<PD5) | (1<<PD7));
       PORTD |= (1<<PD6);
       break;
-    case 4:
+    case 4: // Top right ON, all others OFF
       PORTB &= ~(1<<PB0);
       PORTD &= ~((1<<PD6) | (1<<PD7));
       PORTD |= (1<<PD5);
       break;
-    default:
+    default: // All 7-seg displays OFF
       PORTB &= ~(1<<PB0);
       PORTD &= ~((1<<PD5) | (1<<PD6) | (1<<PD7));
       break;
